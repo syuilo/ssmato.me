b18a9ae7e069852f5635e3263aa3136faf52cff5
syuilo <syuilotan@yahoo.co.jp>
2016-04-03 09:11:05 +0900

    :pizza:

--- src/web/desktop/pages/series/index/controller.ts
+++ src/web/desktop/pages/series/index/controller.ts
@@ -1,36 +1,24 @@
 import * as express from 'express';
 import { Series, SS, Character } from '../../../../../db/models';
 import { ISeries, ISSThread, ICharacter } from '../../../../../db/interfaces';
+import ui from '../../../ui';
 
 module.exports = (req: express.Request, res: express.Response): void => {
 	const series = res.locals.series;
-	SS
-	.find({
-		series: series.id
-	})
-	.sort({
-		registeredAt: -1
-	})
-	.limit(16)
-	.populate('series')
-	.populate('characters', 'name kana screenName aliases color _id')
-	.exec((err: any, sss: ISSThread[]) => {
-		Promise.all(sss.map(ss => {
-			return new Promise<ISSThread>((resove, reject) => {
-				const ssobj: any = ss.toObject();
-				if (ss.series !== []) {
-					Character.find({
-						series: { $in: (<ISeries[]>ss.series).map(x => x.id) }
-					}, 'name kana aliases color _id', (err: any, characters: ICharacter[]) => {
-						resove(ssobj);
-					});
-				} else {
-					resove(ssobj);
-				}
-			});
-		})).then(sss => {
+	ui(req, res).then(() => {
+		SS
+		.find({
+			series: series.id
+		})
+		.sort({
+			_id: -1
+		})
+		.limit(16)
+		.populate('series')
+		.populate('characters.profile', 'name kana screenName aliases color _id')
+		.exec((err: any, sss: ISSThread[]) => {
 			res.locals.display({
-				sss: sss
+				sss: sss.map(ss => ss.toObject())
 			});
 		});
 	});
