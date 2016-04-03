9ff66c7794534f0d4c94acbcc27624833933d82d
syuilo <syuilotan@yahoo.co.jp>
2016-04-04 06:02:52 +0900

    :pizza:

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,14 +3,14 @@
 import {task, src, dest} from 'gulp';
 import * as glob from 'glob';
 import * as ts from 'gulp-typescript';
-//import * as tslint from 'gulp-tslint';
+import * as tslint from 'gulp-tslint';
 import * as browserify from 'browserify';
 const source = require('vinyl-source-stream');
-//const buffer = require('vinyl-buffer');
+const buffer = require('vinyl-buffer');
 const es = require('event-stream');
 const stylus = require('gulp-stylus');
-//const cssnano = require('gulp-cssnano');
-//const uglify = require('gulp-uglify');
+const cssnano = require('gulp-cssnano');
+const uglify = require('gulp-uglify');
 
 task('build', [
 	'build:ts',
@@ -44,8 +44,8 @@ task('build:frontside-scripts', done => {
 			return browserify({ entries: [entry] })
 				.bundle()
 				.pipe(source(entry.replace('src/web', 'resources').replace('.ls', '.js')))
-				//.pipe(buffer())
-				//.pipe(uglify())
+				.pipe(buffer())
+				.pipe(uglify())
 				.pipe(dest('./built'));
 		});
 		es.merge(tasks).on('end', done);
@@ -55,17 +55,15 @@ task('build:frontside-scripts', done => {
 task('build:frontside-styles', ['copy:bower_components'], () => {
 	return src('./src/web/**/*.styl')
 		.pipe(stylus())
-		//.pipe(cssnano({
-		//	safe: true // 高度な圧縮は無効にする (一部デザインが不適切になる場合があるため)
-		//}))
+		.pipe(cssnano({
+			safe: true // 高度な圧縮は無効にする (一部デザインが不適切になる場合があるため)
+		}))
 		.pipe(dest('./built/resources/'));
 });
 
 task('build-copy', ['build:ts', 'build:frontside-scripts', 'build:frontside-styles'], () => {
 	return es.merge(
 		src([
-			'./src/web/**/*.styl',
-			'./src/web/**/*.ts',
 			'./src/web/**/*.jade'
 		]).pipe(dest('./built/web/')),
 		src('./src/resources/**/*').pipe(dest('./built/resources/')),
