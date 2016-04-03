0261f3308183636a81bd82bceaa8d7e89c68b513
syuilo <syuilotan@yahoo.co.jp>
2016-04-04 07:54:30 +0900

    :pizza:

--- src/core/analyzer/extract-name-part-in-serif.ts
+++ src/core/analyzer/extract-name-part-in-serif.ts
@@ -1,3 +1,25 @@
+const Entities = require('html-entities').AllHtmlEntities;
+const entities = new Entities();
+
+const brackets = [
+	'「',
+	'｢',
+	'『',
+	'《',
+	'≪',
+	'（',
+	'('
+];
+
+const bracketsRegExp =
+	brackets
+	.map(b => `\\${b}`)
+	.concat(
+		brackets
+		.map(b => '\\' + entities.encode(b))
+	)
+	.join('|');
+
 /**
  * セリフ行の中のキャラクター名部分を抽出します
  * @param serif セリフ
@@ -10,11 +32,19 @@ export default (serif: string): string => {
 		return null;
 	}
 
-	if (/^(.+?)[（\(].+?[）\)][「｢『《≪（\(].+/.test(serif)) {
-		return serif.match(/^((.+?)[（\(].+?[）\)])[「｢『《≪（\(].+/)[1];
-	} else if (/^(.+?)[「｢『《≪（\(](.+?)/.test(serif)) {
-		return serif.match(/^(.+?)[「｢『《≪（\(](.+?)/)[1];
-	} else {
-		return null;
+	const includeBracketsRegExpMatch =
+		serif.match(new RegExp(`^((.+?)[（\(].+?[）\)])(${bracketsRegExp}).+`));
+
+	if (includeBracketsRegExpMatch !== null) {
+		return includeBracketsRegExpMatch[1];
 	}
+
+	const normalRegExpMatch =
+		serif.match(new RegExp(`^(.+?)(${bracketsRegExp}).+`));
+
+	if (normalRegExpMatch !== null) {
+		return normalRegExpMatch[1];
+	}
+
+	return null;
 }
