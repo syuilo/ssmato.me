c53a7c69eda2021dabb03dabc4acf7f616658726
syuilo <syuilotan@yahoo.co.jp>
2016-04-04 15:07:20 +0900

    Remove unused code

--- src/db/db.ts
+++ src/db/db.ts
@@ -17,46 +17,4 @@ db.on('error', (err: any) => {
 	console.error(`MongoDB connection error: ${err}`);
 });
 
-/*
-//////////////////////////////////////////////////
-// River SETTINGS
-
-Promise.all([
-	// DB
-	mongodb.MongoClient.connect(
-		config.mongo.dbUri, config.mongo.options),
-	// Local
-	mongodb.MongoClient.connect(
-		config.mongo.localUri, config.mongo.options)
-]).then(connections => {
-	const db = connections[0];
-	const local = connections[1];
-
-	const river = River(
-		elasticsearch.Client(),
-		local.collection('oplog.rs'),
-		{
-			'db.ss': { // namespace - 'db.collection'
-				init: null, // for the first run
-				collection: db.collection('ss')
-			}
-		}
-	);
-
-	river.on('error', (err: any) => {
-		throw err;
-	});
-
-	river.run()
-	.then(() => (console.log('River running'), river.init()))
-	.then(() => console.log('River initialized'))
-	.catch((err: any) => {
-		throw err;
-	});
-
-}).catch((err) => {
-	throw err;
-});
-*/
-
 export default db;
