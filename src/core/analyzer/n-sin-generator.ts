223b004d73243e81aa01baca81453f68f20818ae
syuilo <syuilotan@yahoo.co.jp>
2016-04-05 19:00:06 +0900

    Improve readability

--- src/core/analyzer/n-sin-generator.ts
+++ src/core/analyzer/n-sin-generator.ts
@@ -1,15 +1,20 @@
+/**
+ * 十進数を任意の文字列で構成されるn進数文字列に変換します
+ * @param seed 十進数数値
+ * @return n進数
+ */
 export default (seed: number, chars: string): string => {
-
-	let s = '';
+	const n = chars.length;
 
 	if (seed === 0) {
 		return chars[0];
 	}
 
+	let s = '';
+
 	while (seed > 0) {
-		const digit = chars[seed % chars.length];
-		s = digit + s;
-		seed = Math.floor(seed / chars.length);
+		s = chars[seed % n] + s;
+		seed = Math.floor(seed / n);
 	}
 
 	return s;
