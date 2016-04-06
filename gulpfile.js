3416e4fd018bf5d9090acb704e974d4eb527abfe
syuilo <syuilotan@yahoo.co.jp>
2016-04-06 14:58:46 +0900

    Use JavaScript instead of TypeScript

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 /// <reference path="./typings/bundle.d.ts" />
 
-import * as fs from 'fs';
-import {task, src, dest} from 'gulp';
-import * as glob from 'glob';
-import * as ts from 'gulp-typescript';
-import * as tslint from 'gulp-tslint';
-import * as browserify from 'browserify';
+const fs = require('fs');
+const gulp = require('gulp');
+const glob = require('glob');
+const ts = require('gulp-typescript');
+const tslint = require('gulp-tslint');
+const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const es = require('event-stream');
@@ -13,7 +13,7 @@ const stylus = require('gulp-stylus');
 const cssnano = require('gulp-cssnano');
 const uglify = require('gulp-uglify');
 
-task('build', [
+gulp.task('build', [
 	'build:ts',
 	'copy:bower_components',
 	'build:frontside-scripts',
@@ -25,16 +25,16 @@ const project = ts.createProject('tsconfig.json', {
 	typescript: require('typescript')
 });
 
-function buildTypeScript(): ts.CompilationStream {
+function buildTypeScript() {
 	return project.src().pipe(ts(project));
 }
 
-task('build:ts', () =>
+gulp.task('build:ts', () =>
 	buildTypeScript()
-		.pipe(dest('./built/'))
+		.pipe(gulp.dest('./built/'))
 );
 
-task('build:public-config', ['build:ts'], (done: () => void) => {
+gulp.task('build:public-config', ['build:ts'], done => {
 	const config = require('./built/config').default;
 	fs.mkdir('./built/_', e => {
 		if (!e || (e && e.code === 'EEXIST')) {
@@ -45,13 +45,13 @@ task('build:public-config', ['build:ts'], (done: () => void) => {
 	});
 });
 
-task('copy:bower_components', () => {
-	return src('./bower_components/**/*')
-		.pipe(dest('./built/resources/bower_components/'));
+gulp.task('copy:bower_components', () => {
+	return gulp.src('./bower_components/**/*')
+		.pipe(gulp.dest('./built/resources/bower_components/'));
 });
 
-task('build:frontside-scripts', ['build:public-config'], (done: () => void) => {
-	glob('./src/web/**/*.ls', (err: Error, files: string[]) => {
+gulp.task('build:frontside-scripts', ['build:public-config'], done => {
+	glob('./src/web/**/*.ls', (err, files) => {
 		const tasks = files.map(entry => {
 			return browserify({
 				entries: [entry],
@@ -63,38 +63,38 @@ task('build:frontside-scripts', ['build:public-config'], (done: () => void) => {
 				.pipe(source(entry.replace('src/web', 'resources').replace('.ls', '.js')))
 				.pipe(buffer())
 				.pipe(uglify())
-				.pipe(dest('./built'));
+				.pipe(gulp.dest('./built'));
 		});
 		es.merge(tasks).on('end', done);
 	});
 });
 
-task('build:frontside-styles', ['copy:bower_components'], () => {
-	return src('./src/web/**/*.styl')
+gulp.task('build:frontside-styles', ['copy:bower_components'], () => {
+	return gulp.src('./src/web/**/*.styl')
 		.pipe(stylus())
 		.pipe(cssnano({
 			safe: true // 高度な圧縮は無効にする (一部デザインが不適切になる場合があるため)
 		}))
-		.pipe(dest('./built/resources/'));
+		.pipe(gulp.dest('./built/resources/'));
 });
 
-task('build-copy', ['build:ts', 'build:frontside-scripts', 'build:frontside-styles'], () => {
+gulp.task('build-copy', ['build:ts', 'build:frontside-scripts', 'build:frontside-styles'], () => {
 	return es.merge(
-		src([
+		gulp.src([
 			'./src/web/**/*.jade'
-		]).pipe(dest('./built/web/')),
-		src('./src/resources/**/*').pipe(dest('./built/resources/')),
-		src([
+		]).pipe(gulp.dest('./built/web/')),
+		gulp.src('./src/resources/**/*').pipe(gulp.dest('./built/resources/')),
+		gulp.src([
 			'./src/web/**/*',
 			'!./src/web/**/*.js',
 			'!./src/web/**/*.ts',
 			'!./src/web/**/*.ls'
-		]).pipe(dest('./built/resources/'))
+		]).pipe(gulp.dest('./built/resources/'))
 	);
 });
 
-task('lint', () =>
-	src('./src/**/*.ts')
+gulp.task('lint', () =>
+	gulp.src('./src/**/*.ts')
 		.pipe(tslint({
 			tslint: require('tslint')
 		}))
