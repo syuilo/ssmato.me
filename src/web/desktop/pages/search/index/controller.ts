c45b64f7614d654f6be6e7739cc0517b5e65a560
syuilo <syuilotan@yahoo.co.jp>
2016-04-13 11:07:07 +0900

    imfeelinglucky

--- src/web/desktop/pages/search/index/controller.ts
+++ src/web/desktop/pages/search/index/controller.ts
@@ -7,6 +7,7 @@ module.exports = (req: express.Request, res: express.Response): void => {
 	if (req.query.hasOwnProperty('q')) {
 		const q: string = req.query.q;
 		const from: number = parseInt(req.query.from, 10) || 0;
+		const imfeelinglucky = req.query.hasOwnProperty('imfeelinglucky');
 
 		if (q === '') {
 			res.redirect(config.public.urls.search);
@@ -14,11 +15,15 @@ module.exports = (req: express.Request, res: express.Response): void => {
 		}
 
 		search(q, from).then((sss: ISSThread[]) => {
-			res.locals.display({
-				q: q,
-				from: from,
-				sss: sss
-			}, 'search/index/result');
+			if (imfeelinglucky) {
+				res.redirect(config.public.url + '/' + sss[0]._id.toString());
+			} else {
+				res.locals.display({
+					q: q,
+					from: from,
+					sss: sss
+				}, 'search/index/result');
+			}
 		});
 	} else {
 		res.locals.display();
