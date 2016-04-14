ede20ed095f92957357aa2fb7f9a60f2f00ee384
syuilo <Syuilotan@yahoo.co.jp>
2016-04-14 21:21:30 +0900

    Improve intelligence

--- src/core/analyzer/mark-anchor.ts
+++ src/core/analyzer/mark-anchor.ts
@@ -93,6 +93,7 @@ export default
 			/安価(.*?)[下↓]/.test(text) ||
 			text.indexOf('ksk') > -1 ||
 			text.indexOf('支援') > -1 ||
+			text.indexOf('sien') > -1 ||
 			text.indexOf('しえ') > -1 ||
 			text.indexOf('④') > -1 ||
 			text.indexOf('シエンタ') > -1
