3c61d89b96e854311400a5b644842f1057cae69b
syuilo <syuilotan@yahoo.co.jp>
2016-04-15 11:28:55 +0900

    Refactoring :sparkles:

--- src/core/analyzer/identity.ts
+++ src/core/analyzer/identity.ts
@@ -1,6 +1,9 @@
 import { ICharacter } from './interfaces';
 
 import CharacterIdentity from './character-identity';
+import removeWhiteSpaces from './string-utilities/remove-white-spaces';
+import hankakukatakanaToZenkakukatakana from './string-utilities/hankakukatakana-to-zenkakukatakana';
+import katakanaToHiragana from './string-utilities/katakana-to-hiragana';
 
 /**
  * 対象のキャラクターと名前からアイデンティティを生成します。
@@ -102,70 +105,5 @@ export default (character: ICharacter, name: string): CharacterIdentity => {
 }
 
 function normalize(str: string): string {
-	return katakanaToHiragana(hankakukatakanaToZenkakukatakana(removeSpaces(str)));
+	return katakanaToHiragana(hankakukatakanaToZenkakukatakana(removeWhiteSpaces(str)));
 }
-
-function removeSpaces(s: string): string {
-	return s.replace(/\s/g, '');
-}
-
-/**
- * 半角カタカナを全角カタカナに変換
- *
- * @param {String} str 変換したい文字列
- */
-function hankakukatakanaToZenkakukatakana(str: string): string {
-	const kanaMap = {
-		'ｶﾞ': 'ガ', 'ｷﾞ': 'ギ', 'ｸﾞ': 'グ', 'ｹﾞ': 'ゲ', 'ｺﾞ': 'ゴ',
-		'ｻﾞ': 'ザ', 'ｼﾞ': 'ジ', 'ｽﾞ': 'ズ', 'ｾﾞ': 'ゼ', 'ｿﾞ': 'ゾ',
-		'ﾀﾞ': 'ダ', 'ﾁﾞ': 'ヂ', 'ﾂﾞ': 'ヅ', 'ﾃﾞ': 'デ', 'ﾄﾞ': 'ド',
-		'ﾊﾞ': 'バ', 'ﾋﾞ': 'ビ', 'ﾌﾞ': 'ブ', 'ﾍﾞ': 'ベ', 'ﾎﾞ': 'ボ',
-		'ﾊﾟ': 'パ', 'ﾋﾟ': 'ピ', 'ﾌﾟ': 'プ', 'ﾍﾟ': 'ペ', 'ﾎﾟ': 'ポ',
-		'ｳﾞ': 'ヴ', 'ﾜﾞ': 'ヷ', 'ｦﾞ': 'ヺ',
-		'ｱ': 'ア', 'ｲ': 'イ', 'ｳ': 'ウ', 'ｴ': 'エ', 'ｵ': 'オ',
-		'ｶ': 'カ', 'ｷ': 'キ', 'ｸ': 'ク', 'ｹ': 'ケ', 'ｺ': 'コ',
-		'ｻ': 'サ', 'ｼ': 'シ', 'ｽ': 'ス', 'ｾ': 'セ', 'ｿ': 'ソ',
-		'ﾀ': 'タ', 'ﾁ': 'チ', 'ﾂ': 'ツ', 'ﾃ': 'テ', 'ﾄ': 'ト',
-		'ﾅ': 'ナ', 'ﾆ': 'ニ', 'ﾇ': 'ヌ', 'ﾈ': 'ネ', 'ﾉ': 'ノ',
-		'ﾊ': 'ハ', 'ﾋ': 'ヒ', 'ﾌ': 'フ', 'ﾍ': 'ヘ', 'ﾎ': 'ホ',
-		'ﾏ': 'マ', 'ﾐ': 'ミ', 'ﾑ': 'ム', 'ﾒ': 'メ', 'ﾓ': 'モ',
-		'ﾔ': 'ヤ', 'ﾕ': 'ユ', 'ﾖ': 'ヨ',
-		'ﾗ': 'ラ', 'ﾘ': 'リ', 'ﾙ': 'ル', 'ﾚ': 'レ', 'ﾛ': 'ロ',
-		'ﾜ': 'ワ', 'ｦ': 'ヲ', 'ﾝ': 'ン',
-		'ｧ': 'ァ', 'ｨ': 'ィ', 'ｩ': 'ゥ', 'ｪ': 'ェ', 'ｫ': 'ォ',
-		'ｯ': 'ッ', 'ｬ': 'ャ', 'ｭ': 'ュ', 'ｮ': 'ョ',
-		'｡': '。', '､': '、', 'ｰ': 'ー', '｢': '「', '｣': '」', '･': '・'
-	};
-
-	const reg = new RegExp('(' + Object.keys(kanaMap).join('|') + ')', 'g');
-	return str
-		.replace(reg, match =>
-			kanaMap[match]
-		)
-		.replace(/ﾞ/g, '゛')
-		.replace(/ﾟ/g, '゜');
-};
-
-/**
- * カタカナをひらがなに変換します
- * @param {String} str - カタカナ
- * @returns {String} - ひらがな
- */
-function katakanaToHiragana(str: string): string {
-	return str.replace(/[\u30a1-\u30f6]/g, match => {
-		const char = match.charCodeAt(0) - 0x60;
-		return String.fromCharCode(char);
-	});
-}
-
-///**
-// * ひらがなをカタカナに変換します
-// * @param {String} str - ひらがな
-// * @returns {String} - カタカナ
-// */
-//function hiraganaToKatagana(str: string): string {
-//	return str.replace(/[\u3041-\u3096]/g, match => {
-//		const char = match.charCodeAt(0) + 0x60;
-//		return String.fromCharCode(char);
-//	});
-//}
