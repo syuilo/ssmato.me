36f5b380e76527731d8ce163d38624b8afeed837
syuilo <syuilotan@yahoo.co.jp>
2016-04-15 16:10:16 +0900

    Fix type

--- src/core/analyzer/string-utilities/hankakukatakana-to-zenkakukatakana.ts
+++ src/core/analyzer/string-utilities/hankakukatakana-to-zenkakukatakana.ts
@@ -5,7 +5,7 @@
  * @returns {String} 全角カタカナ
  */
 export default function(str: string): string {
-	const kanaMap = {
+	const kanaMap: any = {
 		'ｶﾞ': 'ガ', 'ｷﾞ': 'ギ', 'ｸﾞ': 'グ', 'ｹﾞ': 'ゲ', 'ｺﾞ': 'ゴ',
 		'ｻﾞ': 'ザ', 'ｼﾞ': 'ジ', 'ｽﾞ': 'ズ', 'ｾﾞ': 'ゼ', 'ｿﾞ': 'ゾ',
 		'ﾀﾞ': 'ダ', 'ﾁﾞ': 'ヂ', 'ﾂﾞ': 'ヅ', 'ﾃﾞ': 'デ', 'ﾄﾞ': 'ド',
