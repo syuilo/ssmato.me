5b3158cc38f33a622017f52c33adf98b18a44920
syuilo <syuilotan@yahoo.co.jp>
2016-04-30 12:15:54 +0900

    [WIP] use Homula :package:

--- src/core/analyzer/extract-anchors.ts
+++
@@ -1,1 +0,0 @@
-export default (text: string) => text.match(/(>>|＞＞)([0-9０-９\-]+)/g);
--- src/core/analyzer/string-utilities/remove-white-spaces.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 文字列内のスペースを除去します
- *
- * @param {String} str
- * @returns {String}
- */
-export default function(s: string): string {
-	return s.replace(/\s/g, '');
-}
